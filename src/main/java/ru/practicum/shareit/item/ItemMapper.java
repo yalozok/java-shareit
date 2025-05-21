25059e941b9139ad9a85537cf29d42816db4702f
yalozok <olga.kozmovskaia@yandex.ru>
2025-05-21 12:10:26 +0200

    Add ItemModel and ItemDto

---
+++ src/main/java/ru/practicum/shareit/item/ItemMapper.java
@@ -0,0 +1,27 @@
+package ru.practicum.shareit.item;
+
+import org.springframework.stereotype.Component;
+import ru.practicum.shareit.item.dto.ItemCreateDto;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Item;
+
+@Component
+public class ItemMapper {
+    public static Item toModel(ItemCreateDto itemDto, Long ownerId) {
+        Item item = new Item();
+        item.setName(itemDto.getName());
+        item.setDescription(itemDto.getDescription());
+        item.setAvailable(itemDto.getAvailable());
+        item.setOwner(ownerId);
+        return item;
+    }
+
+    public static ItemDto toDto(Item item) {
+        ItemDto itemDto = new ItemDto();
+        itemDto.setId(item.getId());
+        itemDto.setName(item.getName());
+        itemDto.setDescription(item.getDescription());
+        itemDto.setAvailable(item.isAvailable());
+        return itemDto;
+    }
+}
--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,14 @@
 package ru.practicum.shareit.item.dto;
 
+import lombok.Data;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
 public class ItemDto {
+    private long id;
+    private String name;
+    private String description;
+    private boolean available;
 }
