a8a335e90fea34f770e88b5e2f19ce77954a4af5
yalozok <olga.kozmovskaia@yandex.ru>
2025-06-07 23:29:50 +0200

    Add Booking Entity

---
+++ src/main/java/ru/practicum/shareit/booking/model/Booking.java
@@ -0,0 +1,44 @@
+package ru.practicum.shareit.booking.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.User;
+
+import java.time.LocalDateTime;
+
+/**
+ * TODO Sprint add-bookings.
+ */
+@Entity
+@Table(name = "bookings")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "start_date_time")
+    private LocalDateTime start;
+
+    @Column(name = "end_date_time")
+    private LocalDateTime end;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "booker_id")
+    private User booker;
+
+    @Convert(converter = BookingStatusConverter.class)
+    private BookingStatus status;
+}
