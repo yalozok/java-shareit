26319ee21b5e224a661d78041b3d29e1e93227d3
yalozok <olga.kozmovskaia@yandex.ru>
2025-06-17 20:09:04 +0200

    Split project to modules: updateExceptionHandlers

--- server/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ server/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -1,19 +1,13 @@
 package ru.practicum.shareit.exception;
 
 import com.fasterxml.jackson.databind.exc.ValueInstantiationException;
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.stream.Collectors;
 
 @RestControllerAdvice
 @Slf4j
@@ -49,29 +43,10 @@ public class ErrorHandler {
         return new ErrorResponse(e.getMessage());
     }
 
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public void handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        String message = e.getBindingResult().getAllErrors().stream()
-                .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                .collect(Collectors.joining(", "));
-
-        throw new ValidationException(message);
-    }
-
     @ExceptionHandler({IllegalArgumentException.class, ValueInstantiationException.class,
             HttpMessageNotReadableException.class, MissingRequestHeaderException.class})
     public void handleIllegalArgumentException(Exception e) {
         throw new ValidationException(e.getMessage());
-    }
-
-    @ExceptionHandler(ConstraintViolationException.class)
-    public void handleConstraintViolationException(ConstraintViolationException e) {
-        String message = e.getConstraintViolations().stream()
-                .map(ConstraintViolation::getMessage)
-                .collect(Collectors.joining(", "));
-
-        throw new ValidationException(message);
     }
 
     @ExceptionHandler
